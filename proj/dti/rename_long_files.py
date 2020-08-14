46877b9df4a6c77fbf88eee7cf791c7bad957cc8
bbrighttaer <brighteragyemang@gmail.com>
2020-08-15 01:31:22 +0800

    sim data

--- proj/dti/rename_long_files.py
+++ proj/dti/rename_long_files.py
@@ -5,17 +5,21 @@
 # File: rename_long_files.py
 
 
-from __future__ import print_function
 from __future__ import division
+from __future__ import print_function
 from __future__ import unicode_literals
 
-import os
 import json
+import os
+import pandas as pd
+
+from tqdm import tqdm
 
 if __name__ == '__main__':
     json_dir = './analysis'
     json_files = []
-    for root, directories, file in os.walk(json_dir):
+    success_count = 0
+    for root, directories, file in tqdm(os.walk(json_dir)):
         if len(file) > 0:
             for f in file:
                 if '}.json' in f:
@@ -25,11 +29,32 @@ if __name__ == '__main__':
                                              long_filename_dict['split'],
                                              long_filename_dict['date'],
                                              '.json'])
-                    with open(os.path.join(root, f), 'r') as data_file:
-                        print(root, data_file)
-                        json_data = json.load(data_file)
-                    json_data['metadata'] = long_filename_dict
-                    with open(os.path.join(root, new_filename), 'w') as data_file:
-                        json.dump(dict(json_data), data_file)
-                    os.remove(os.path.join(root, f))
-
+                    try:
+                        with open(os.path.join(root, f), 'r') as data_file:
+                            json_data = json.load(data_file)
+                        json_data['metadata'] = long_filename_dict
+                        with open(os.path.join(root, new_filename), 'w') as data_file:
+                            json.dump(dict(json_data), data_file)
+                        os.remove(os.path.join(root, f))
+                        success_count += 1
+                    except Exception as e:
+                        print(os.path.join(root, f))
+                        print(str(e))
+                elif '}.csv' in f:
+                    long_filename_dict = json.loads(f.split(".c")[0])
+                    new_filename = '_'.join([long_filename_dict['model_family'],
+                                             long_filename_dict['dataset'],
+                                             long_filename_dict['model_ds'],
+                                             long_filename_dict['cviews'],
+                                             long_filename_dict['pviews'],
+                                             long_filename_dict['split'],
+                                             long_filename_dict['date'],
+                                             '.csv'])
+                    try:
+                        df = pd.read_csv(os.path.join(root, f))
+                        df.to_csv(os.path.join(root, new_filename), index=False)
+                        os.remove(os.path.join(root, f))
+                        success_count += 1
+                    except Exception as e:
+                        print(os.path.join(root, f))
+    print(success_count)
